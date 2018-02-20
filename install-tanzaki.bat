58a0ee706a3e592db390ad256f0eaaca16ebe648
hao <qtvhao@users.noreply.github.com>
2018-02-21 05:37:36 +0700

    Add files via upload

--- install-tanzaki.bat
+++ install-tanzaki.bat
@@ -1,5 +1,5 @@
 "./dist/php/php.exe" "./dist/composer.phar" config -g -- disable-tls true
 "./dist/php/php.exe" "./dist/composer.phar" config -g -- secure-http false
 
-"./dist/php/php.exe" -n -dextension="%cd%/dist/php/ext/php_mbstring.dll" "./dist/composer.phar" create-project --prefer-dist laravel/laravel practice-original
+"./dist/php/php.exe" -n -dextension="%cd%/dist/php/ext/php_mbstring.dll" -dextension="%cd%/dist/php/ext/php_openssl.dll" -dextension="%cd%/dist/php/ext/php_pdo_sqlite.dll" -dextension="%cd%/dist/php/ext/php_xmlrpc.dll" "./dist/composer.phar" create-project --prefer-dist laravel/laravel practice-original
 set /p temp="Hit enter to continue"
